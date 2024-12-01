411e2c110b86c87f329c9f894cfcc263b3ec0e55
conzume10 <leechangzhe@gmail.com>
2024-12-01 22:25:01 +0800

    modified:   __pycache__/chef.cpython-312.pyc 	modified:   chef.py

--- chef.py
+++ chef.py
@@ -103,7 +103,7 @@ food_inv = {
 
 # 1.2.2 添加菜品
 def add_recipe():
-    print("Please insert your recipe")
+    print("Insert the name of the food or drink to add")
     print("Press Enter without typing anything to finish.")
 
     while True:
